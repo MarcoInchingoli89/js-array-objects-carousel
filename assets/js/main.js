ac50a7794b65673942f638dc27e89bf6cf0adc60
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-21 15:21:54 +0200

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+/* Consegna:
+Dato un array di oggetti letterali con:
+url dell’immagine
+titolo
+descrizione Creare un carosello come nella foto allegata.
+Milestone 0:
+Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+Milestone 1:
+Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+Milestone 2:
+Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra. */
+
+/* STRUTTURA DATI:
+Ecco la struttura dati da usare (le immagini sono le stesse della volta scorsa)
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+]; */
+
+// Creare un array con le immagini
+const images = [
+    './assets/img/01.webp',
+    './assets/img/02.webp',
+    './assets/img/03.webp',
+    './assets/img/04.webp',
+    './assets/img/05.webp'
+];
+
+//Selezioniamo il div dove vogliamo inserire le immagini
+const slidesEl = document.querySelector('.slides');
+
+//Crea un ciclo for con le immagini
+for (let i = 0; i < images.length; i++) {
+    const imagesEl = images[i];
+    //Vado a selezionare il mio markup
+    const slidesMarkUp = `<img width="500" src="${imagesEl}" alt="">`
+    //Stampo su console il markup
+    console.log(slidesMarkUp);
+    //Stampo su pagina il mio markup
+    slidesEl.insertAdjacentHTML('beforeend', slidesMarkUp);
+    //Stampo su console tutte le immagini
+    /* console.log(imagesEl); */
+    
+}
+
+// Selezioniamo la prima immagine di quelle inserite in pagina
+const imgActive = document.images[0];
+//Applica la classe "active" alla prima immagine
+imgActive.className = "active";
+console.log(imgActive)
